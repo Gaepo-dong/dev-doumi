0df306a0a978ca9c5bc88fe39a02a91b13147b77
SeoJunhwan <seojunhwan@kakao.com>
2022-12-22 22:08:06 +0900

    feat: extract tag constants

---
+++ constants/tags.ts
@@ -0,0 +1,17 @@
+import { mainTagIdentifier, subTagIdentifier } from '@/@types';
+
+export const subTags: subTagIdentifier[] = [
+  'iTerm',
+  'IDE',
+  'browser',
+  'widget',
+  'application',
+  'system',
+  'mouse',
+  'keyboard',
+  'monitor',
+  'laptop',
+  'headphone',
+];
+
+export const mainTags: mainTagIdentifier[] = ['trending', 'developer', 'mac', 'hardware'];
